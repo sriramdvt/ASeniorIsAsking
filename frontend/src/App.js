122fa630ac04e14f8b33fd53abe4def4943f29fe
Sriram <41079588+sriramdvt@users.noreply.github.com>
2020-05-23 20:03:46 +0530

    NavBar (#5)
    * Added Navbar
    
    * Added Proper Navbar
    
    * Added Proper Navbar
    
    * Fixed Button Capitalization
    
    * Added Card

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import ReactDOM from 'react-dom';
 import logo from "./logo.svg";
 import "./App.css";
 
@@ -29,4 +30,5 @@ function App() {
   );
 }
 
-export default App;
+
+export default Navbar;
