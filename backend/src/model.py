e93401ddb12b3bb58c3b1de3bfe9133a51bf3cdf
Ishaan Shah <ishaanshah@users.noreply.github.com>
2020-05-23 09:03:54 +0530

    Add basic routes and their implemetation
    * Request and Accept endpoints with redis data handling
    
    * Fix formatting and some minor issues
    
    Authored-by: rahul-goel <rahulgoelko@gmail.com>
    Co-authored-by: ishaanshah <ishaan.n.shah@gmail.com>

--- backend/src/model.py
+++ backend/src/model.py
@@ -1,4 +1,5 @@
 import redis
+import json
 
 import backend.src.config as config
 
@@ -7,12 +8,55 @@ class RedisConnection():
     def __init__(self):
         self.conn = redis.Redis(host=config.REDIS_HOST,
                                 port=config.REDIS_PORT, charset="utf-8", decode_responses=True)
+        self.conn.incr("number_of_orders", amount=0)
 
     def inc_count(self):
         self.conn.incr('count', amount=1)
 
     def get_count(self):
         return self.conn.get('count')
+
+    def valid_order(self, order_id):
+        try:
+            num = int(order_id)
+        except ValueError:
+            return False
+        return (num <= int(self.conn.get("number_of_orders")) and num >= 1)
+
+    def pending_order_add(self, incoming_json):
+        self.conn.incr("number_of_orders", amount=1)
+        latest_order_id = self.conn.get("number_of_orders")
+        self.conn.hset("pending_orders", key=latest_order_id,
+                       value=incoming_json)
+        return latest_order_id
+
+    def get_particular_pending(self, order_id):
+        return self.conn.hget("pending_orders", order_id)
+
+    def get_all_pending(self):
+        return self.conn.hgetall("pending_orders")
+
+    def get_particular_done(self, order_id):
+        return self.conn.hget("accepted_orders", order_id)
+
+    def get_all_done(self):
+        return self.conn.hgetall("accepted_orders")
+
+    def has_it_been_accepted(self, order_id):
+        if(self.conn.hget("accepted_orders", order_id)):
+            return True
+        else:
+            return False
+
+    def accept_order(self, order_id, accepter_json):
+        orderer_json = self.conn.hget("pending_orders", order_id)
+        dict_a = json.loads(orderer_json)
+        dict_b = json.loads(accepter_json)
+        merged_dict = dict_a.copy()
+        merged_dict.update(dict_b)
+        merged_json = json.dumps(merged_dict)
+        self.conn.hset("accepted_orders", key=order_id, value=merged_json)
+        self.conn.hdel("pending_orders", order_id)
 
 
 _redis = None
--- backend/src/views.py
+++ backend/src/views.py
@@ -1,6 +1,8 @@
 from flask import Blueprint
+from flask import request
 
 import backend.src.model as model
+import json
 
 api_bp = Blueprint('api_bp', __name__)
 
@@ -10,3 +12,80 @@ def index():
     model._redis.inc_count()
     count = model._redis.get_count()
     return "This page has been visited {} times".format(count)
+
+
+@api_bp.route("/order_request", methods=["POST"])
+def order_request():
+
+    test_dict = {
+        "key_example": "value_example"
+    }
+
+    latest_order_id = model._redis.pending_order_add(json.dumps(test_dict))
+    return_dict = {
+        "order_id": latest_order_id
+    }
+    return json.dumps(return_dict), 200
+
+    if request.is_json:
+        incoming_json = request.get_json()
+        latest_order_id = model._redis.pending_order_add(incoming_json)
+        return latest_order_id
+    else:
+        return_dict = {
+            "message": "JSON not received."
+        }
+        return json.dumps(return_dict), 400
+
+
+@api_bp.route("/pending_orders_list", methods=["GET"])
+def pending_orders():
+    pending_order_dict = model._redis.get_all_pending()
+    return json.dumps(pending_order_dict)
+
+
+@api_bp.route("/accepted_orders", methods=["GET"])
+def accepted_orders():
+    accepted_orders_dict = model._redis.get_all_done()
+    return json.dumps(accepted_orders_dict)
+
+
+@api_bp.route("/accepted_orders/<order_id>", methods=["GET"])
+def particular_order(order_id):
+    if model._redis.valid_order(order_id) is False:
+        return_dict = {
+            "message": "Invalid Order ID."
+        }
+        return json.dumps(return_dict), 404
+    elif model._redis.has_it_been_accepted(order_id):
+        accepted_json = model._redis.get_particular_done(order_id)
+        return accepted_json, 200
+    else:
+        return_dict = {
+            "message": "Your order is still pending."
+        }
+        return json.dumps(return_dict), 200
+
+
+@api_bp.route("/accept_order", methods=["POST"])
+def accept_order():
+    if request.is_json:
+        accepter_dict = request.get_json()
+        order_id = accepter_dict["order_id"]
+        if model._redis.has_it_been_accepted(order_id):
+            return_dict = {
+                "message": "The order has already been accepted by someone else."
+            }
+            return json.dumps(return_dict), 203
+        else:
+            model._redis.accept_order(order_id, json.dumps(accepter_dict))
+            return_dict = {
+                "message": "You have accepted the order."
+            }
+            return json.dumps(return_dict), 200
+
+    else:
+        return_dict = {
+            "message": "JSON not received."
+        }
+        return json.dumps(return_dict), 400
